bcc04057d03d1fb3be61080cabfc4dc1a596daa4
Mark <159958226+Markayo21@users.noreply.github.com>
2025-05-04 14:06:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from pet import Pet
+
+# Create your digital pet
+buddy = Pet("Buddy")
+
+# Interact with the pet
+buddy.get_status()
+
+buddy.eat()
+buddy.sleep()
+buddy.play()
+
+buddy.get_status()
+
+# Bonus: Teach tricks
+buddy.train("roll over")
+buddy.train("sit")
+
+buddy.show_tricks()
+buddy.get_status()
+
